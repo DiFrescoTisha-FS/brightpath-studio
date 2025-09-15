d7d9d7df3ac6ff826bbb34c8df254aa094a3fe03
DiFrescoTisha-FS <81319127+DiFrescoTisha-FS@users.noreply.github.com>
2025-09-15 16:03:56 -0400

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import HomePage from './components/pages/HomePage';
+import ContactPage from './components/pages/ContactPage'
 
 function App() {
-  return <HomePage />;
+  return (
+     <HomePage />;
+     <ContactPage />;
+  )
 }
 
 export default App;
